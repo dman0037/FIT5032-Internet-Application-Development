3aa3ab66b22dcf8b7db3bb14ea8ca5d9d2074078
Deepak Mandya Chaluvaiah <dman0037@student.monash.edu>
2024-08-04 23:26:04 +1000

    Initial NyName.vue commit for FIT5032

---
+++ src/components/MyName.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Deepak'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
